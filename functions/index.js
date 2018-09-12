227b98116e8b35c5ebaa7643bfa8c43deed6fe73
Gnorme <thompsoncwt@gmail.com>
2018-09-11 21:42:17 -0400

    updated function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 exports.keyboard = (req, res) => {
+    res.header('Content-Type','application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    //respond to CORS preflight requests
 	var boardType = req.query.board
 	var keyboard = {}
 	fs.readFile(__dirname+'/'+boardType+'.json','utf-8', (err, jData) => {
